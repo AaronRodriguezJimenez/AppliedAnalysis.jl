e7f6e3914aeb86c55929fa21a4c965f8599e48c0
aaronrodriguez <aarodjim@gmail.com>
2025-05-30 18:08:29 -0400

    Initial Commit

--- src/MyFirstPackage.jl
+++ src/MyFirstPackage.jl
@@ -4,20 +4,24 @@
 3.- then in julia> type using Revise and using MyFirstPackage
 This will allow you to have access to whateve you define in the module
 don't forget to save all changes. If there are non declaration errors,
-restart the REPL
-=# 
+restart the REPL and repeat steps 2 and 3.
 
+If nothing works, try to rease data from cache:
+In the bash terminal: rm -rf ~/.julia/compiled/v1.11/MyFirstPackage
+And restart everything.
+=# 
 
 module MyFirstPackage
 
+using Plots
 export greet
 export plot_xy
 
 """ GREETINGS """
 greet() = print("Hello From Revise!")
 
-function plot_xy(x::Array, y::Array)
-    using plots
+function plot_xy(x::AbstractVector, y::AbstractVector)
+    
     plot(x,y, label="Test plot")
 end 
 
--- test/runtests.jl
+++ test/runtests.jl
@@ -1,4 +1,11 @@
+#=
+ USAGE: 
+ julia> ]
+(MyFirstPackage) pkg> test
+
+=#
 using Test
 using MyFirstPackage
 
 @test greet() == nothing # greet() imprime pero no retorna nada
+@test plot_xy(1:10, rand(10)) == nothing

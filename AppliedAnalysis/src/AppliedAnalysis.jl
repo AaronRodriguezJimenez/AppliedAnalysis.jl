91b2a0739556c9210071d8e4e085d871fee933d3
Jose Aaron Rodriguez Jimenez <aarodjim@gmail.com>
2025-09-25 08:08:44 -0400

    Initial commit for AppliedAnalysis.

---
+++ AppliedAnalysis/src/AppliedAnalysis.jl
@@ -0,0 +1,5 @@
+module AppliedAnalysis
+
+greet() = print("Hello World!")
+
+end # module AppliedAnalysis
--- src/memory_analysis.jl
+++
@@ -1,4 +0,0 @@
-using StatProfilerHTML
-using TypedPolynomials
-@polyvar x y z
-@profilehtml (x + y + z)^120;
